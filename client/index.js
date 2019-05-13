39402b6dd9d795173d49389e5ab800161d7e65d0
hyunalee <dlgusdk419@naver.com>
2019-05-13 17:44:00 +0900

    test(polyfill): core-js

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 import 'raf/polyfill';
 
 // ReactDOM.render(<App/>, document.getElementById('root'));
