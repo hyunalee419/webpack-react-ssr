388d83b6f58f740a0ddc7682acd8e1a1f437e168
hyunalee <dlgusdk419@naver.com>
2019-05-13 17:39:07 +0900

    test(raf): import raf

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'raf/polyfill';
 
 // ReactDOM.render(<App/>, document.getElementById('root'));
 ReactDOM.hydrate(<App data={window.__PRELOADED_STATE__}/>, document.getElementById('root'));
